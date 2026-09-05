4033a215f7f474b40cd08559521e112bcde92987
valadaptive <valadaptive@protonmail.com>
2026-09-05 14:13:51 -0400

    Version 0.9.5

--- crates/ae-plugin/build.rs
+++ crates/ae-plugin/build.rs
@@ -8,7 +8,7 @@ fn main() {
 
         const EFFECT_VERSION_MAJOR: u32 = 1;
         const EFFECT_VERSION_MINOR: u32 = 5;
-        const EFFECT_VERSION_PATCH: u32 = 4;
+        const EFFECT_VERSION_PATCH: u32 = 5;
         use pipl::*;
         pipl::plugin_build(vec![
             Property::Kind(PIPLType::AEEffect),
